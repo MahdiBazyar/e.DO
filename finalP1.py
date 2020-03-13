d53229513683cf0ccc80da724d5a48357e014c0a
nesmith4 <60016585+nesmith4@users.noreply.github.com>
2020-03-13 15:27:31 -0400

    Latest version of finalP1.py
    3-13-20
    3:27 PM

--- finalP1.py
+++ finalP1.py
@@ -2,14 +2,12 @@ from collections import deque
 from imutils.video import VideoStream
 import numpy as np
 
-#imutil function is to make basic image processing functions such as 
-#translation, rotation, resizing, skeletonization, displaying Matplotlib images, 
-#sorting contours, detecting edges, and much more easier with OpenCV.
 import cv2
-import imutils  
+import imutils
 import time
 import math
 import sys
+import os
 
 #Adding file to Python PATH to callable elsewhere
 #sys.path.append(".")
@@ -23,55 +21,53 @@ import sys
 #def getObjectLocation():
 pts = deque(maxlen=32)
 cap = cv2.VideoCapture(1) #number is which camera on your system you want to use
-
-# cap.set is used to set the exposure time for (3) width and (4) height
-image_width = 800
-image_height = 600
-cap.set(3, image_width)
-cap.set(4, image_height)
+cap.set(3, 800)
+cap.set(4, 600)
 
 #Allow the camera or video file to warm up
 time.sleep(2.0)
 
-#Math logic to find the height and length of the object detected by open CV
+#Math logic to find the height and length
+depthOrAdjacent = 107
 
-depthOfCamera = 107  #centimeters from floor to camera
+verticalFieldOfView = 43.3
+verticalFieldOfViewHalf = verticalFieldOfView / 2
+baseOrOppositeVFOV = math.tan(math.radians(verticalFieldOfViewHalf)) * depthOrAdjacent
 
-verticalFieldOfView_degrees = 43.3
-verticalFieldOfViewHalf = verticalFieldOfView_degrees / 2
-baseOrOppositeVFOV = math.tan(math.radians(verticalFieldOfViewHalf)) * depthOfCamera
+horizontalFieldOfView = 70.42
+horizontalFieldOfViewHalf = horizontalFieldOfView / 2
+baseOrOppositeHFOV = math.tan(math.radians(horizontalFieldOfViewHalf)) * depthOrAdjacent
 
-horizontalFieldOfView_degrees = 70.42
-horizontalFieldOfViewHalf = horizontalFieldOfView_degrees / 2
-baseOrOppositeHFOV = math.tan(math.radians(horizontalFieldOfViewHalf)) * depthOfCamera
-
+  
 heightIn = baseOrOppositeVFOV * 2
 widthIn = baseOrOppositeHFOV * 2
 print(heightIn, widthIn) #Debug Statement
-
-
-count = 20
+count = 0
+runCamera = True
+os.system('cls||clear')   #to clear the termnial screen
 print("===============")
 print("Camera Terminal")
 print("===============")
+print("Press Ctrl + C to exit.")
 
 while True:
+  while(runCamera):
     #print(count)
-    if(count < 0):
-      temp = raw_input("Type (y) to get object location or (n) to exit: ")
-      if(temp == "y"):
-        count = 20
-      if(temp == "n"):
-        exit(0)
-    while(count >=0):
-      count = count -1
+   # if(count < 0):
+    #  temp = raw_input("Type (y) to get object location or (n) to exit: ")
+   #   if(temp == "y"):
+     #   count = 20
+    #  if(temp == "n"):
+    #    exit(0)
+   # while(count >=0):
+   #   count = count -1
       #Ask the user to continue
       #response = raw_input("To terminate put 'n': \n")
       #if(response == "n"):
         #cap.release()
         #cv2.destroyAllWindows()
         #break;
-      #count = count - 1
+      count = count + 1
       ret, img = cap.read()
       #start code here
       #Location of variable changed to fix results
@@ -81,9 +77,8 @@ while True:
       img = img[10:-10,10:-210]
 
       height, width, _ = img.shape
-      inchesPerPixelH = heightIn / height   #'''heightIn'''
-      inchesPerPixelW = widthIn / width	    #'''widthIn'''
-
+      inchesPerPixelH = heightIn / height #'''heightIn''' 
+      inchesPerPixelW = widthIn / width	#'''widthIn'''
       #ends here
       gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
       #
@@ -95,9 +90,9 @@ while True:
     
       # Apply Hough transform on the blurred image. 
       detected_circles = cv2.HoughCircles(gray_blurred,  
-                     	cv2.HOUGH_GRADIENT, 1, 20, param1 = 50, 
-                 	param2 = 30, minRadius = 1, maxRadius = 40) 
-    
+                     	cv2.HOUGH_GRADIENT, 0.1, 50, param1 = 50, 
+                 	param2 = 40, minRadius = 0, maxRadius = 55) 
+
     # Draw circles that are detected. 
       if detected_circles is not None: 
     
@@ -108,7 +103,7 @@ while True:
               a, b, r = pt[0], pt[1], pt[2] 
     
               # Draw the circumference of the circle. 
-              cv2.circle(img, (a, b), r, (0, 255, 0), 2) 
+              cv2.circle(img, (a, b), r, (204, 193, 255), 2) 
     
               # Draw a small circle (of radius 1) to show the center. 
               cv2.circle(img, (a, b), 1, (0, 0, 255), 3)
@@ -117,43 +112,73 @@ while True:
               # loop over the set of tracked points
               for i in np.arange(1, len(pts)):
                   # coordinates to display/ forward to the ai
+                  diff = 7.25#8#0; #8
+                  #print(r) 
+                  x = round(diff + (pts[0][0] * inchesPerPixelH), 2)
+                  y = round(((heightIn/2) - (pts[0][1] * inchesPerPixelH) + 4.5), 2)
+                  z = round((r * (inchesPerPixelH)) * 2, 2)
+                  #BEING: MATH TO CALCULATE DEPTH OF OBJECT - Hawraa Banoon 
+                  #Source: https://www.pyimagesearch.com/2015/01/19/find-distance-camera-objectmarker-using-python-opencv/
+                  if (r > 0):
+                    #4 Cm by 4 Cm object 
+                    #Depth Perception calculated by: D = (W x F) / P 
+                    #D = Depth, F = Focal Length, P = pixels at certain length 
+                    #In this case F = (P X D) / W , Where P = 16 pixels when on table, 
+                    #D is depth of table (107 cm) and W is 4cn object 
+                    newZ = round((4 * 856) / (r * 2), 2) 
+                    newZ = 107 - newZ
+                  else:
+                    newZ = 6
+                    #print(newZ) 
+                  #END: MATH TO CALCULATE DEPTH OF OBJECT - Hawraa Banoon 
 
-                  edo_base_diff_cm = 7.25 #8#0; #8
-
-                  x = round(edo_base_diff_cm + (pts[0][0] * inchesPerPixelH), 2) #from edo towards camera
-                  y = round(((heightIn/2) - (pts[0][1] * inchesPerPixelH) + 4.5), 2) #from edo perpendicular to camera
-                  z = round((r * inchesPerPixelH) * 2, 2) #from base of edo towards the ceiling
-
-                  newX = round(math.sqrt((x**2) + (y**2)), 2) #euclidean distance to object
-                  xy = math.degrees(math.acos(((x**2) + (newX * newX) - (y**2)) / (2 * x * newX))) #degrees rotation to object
-
+                  newX = round(math.sqrt((x*x) + (y*y)), 2)
+                  print("x:", x) 
+                  print("y:", y) 
+                  xy = math.degrees(math.acos(((x*x) + (newX*newX) - (y*y)) / (2 * x * newX)))
+                  print("newX:", newX) 
+                  #print("xy:", xy)
+                  newXY = math.degrees(math.atan((y/x)))
+                  print("newXY:", newXY)
                   #cv2.line(img, pts[i - 1], pts[i], (0, 0, 255), 3)
 
-                  #if object is to the left of the e.Do, reverse the coordinates
+                  # show the image coordinates
                   if(y<0):
-                    xy = xy * -1
-                    # show the image coordinates
+                    xy = xy * -1 
                   cv2.putText(img, "X: {}, Y: {}, OW: {}".format(x, y, z),
                   	(10, img.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
                   	0.35, (0, 0, 255), 1)
+                  if(count % 1 == 0): 
+                    if(x > 9):
+                      f = open("newX.txt", "w")
+                      print("Updated newX.txt") 
+                      f.write('%f' % newX)
+                      f.close()               
+                      f = open("xy.txt", "w")
+                      print("Updated xy.txt") 
+                      f.write('%f' % xy)
+                      f.close()
+                    #Write to text file the Z value if it is greater than Zero so it doesn't hit table - Hawraa Banoon 
+                    if(newZ > 7.0):
+                      f = open("newZ.txt", "w")
+                      print("Updated newZ.txt") 
+                      f.write('%f' % newZ)
+                      f.close()
+                    else: 
+                      newZ = 6.0
+                      f = open("newZ.txt", "w")
+                      print("Updated newZ.txt") 
 
-                  #if its less than 9 cm away from the origin, e.Do can not pick up the object,
-                  # so only consider values greater than 9
-                  if(x > 9):
-                    f = open("newX.txt", "w")
-                    f.write('%f' % newX)
-                    f.close()               
-                    f = open("xy.txt", "w")
-                    f.write('%f' % xy)
-                    f.close()
+                      f.write('%f' % newZ)
+                      f.close()
 
       cv2.imshow("Detected Circle", img)
-      #cv2.imshow("blurred", gray_blurred)
+      if(count % 50 ==0 and detected_circles is not None):
+        time.sleep(10)
+        cv2.imshow("Detected Circle", gray_blurred)
       #cv2.imshow("gray", gray)
-
-
       k = cv2.waitKey(30) & 0xff
       if k == 27:
-          cap.release()
-          # close all windows
-          cv2.destroyAllWindows()
+           cap.release()
+     # close all windows
+           cv2.destroyAllWindows()
