782d8f16d5360ddeb6fd40e17397f4564cf0ebc8
hawraabanoon <60077926+hawraabanoon@users.noreply.github.com>
2020-03-24 22:45:31 -0400

    Update color_finalP1.py

--- color_finalP1.py
+++ color_finalP1.py
@@ -1,43 +1,48 @@
-# Activates the camera and object detection using colors
-# belongs in direcrory /home/nvidia/jetson-reinforcement/build/aarch64
+# ================================== Camera File! ==================================
+#  Developers: Wayne State University Senior Capstone Project Students
+#              Fall 2019: Adel Mohamed, Corey Simms, Mahmoud Elmasri, & Tyler Riojas 
+#              Winter 2020: Hawraa Banoon, Nathaniel Smith, Kristina Stevoff, & Mahdi Bazyar
+#  Directory:  /home/nvidia/jetson-reinforcement/build/aarch64
+#  Interface:  Runs the camera terminal 
+#  Purpose:    This file runs the camera and allows the user to choose capturing a
+#              single shot of 50 frmaes or run the camera continuously. The images 
+#              will then be processed to output the color and location of the object.
+#  Inputs:     Camera connection
+#  Outputs:    4 Text Files
+#              Color.txt (The color of the object- red, blue, or green) 
+#              newX.txt  (The distance of object from base of e.DO in cm) 
+#              xy.txt    (The angle that the object is in relation to e.DO in cm) 
+#              newZ.txt  (The depth of the object in cm) 
+# ================================== Camera File! ==================================
 
-from collections import deque
-from imutils.video import VideoStream
-import numpy as np
 
-import cv2
-import imutils
-import time
-import math
-import sys
-import os
+# =========== Standard Imports and Necessary Packages ===========
+from collections import deque # import deque module provides you with a double ended queue
+from imutils.video import VideoStream # Capture a live video stream 
+import numpy as np # N-Demensional array-processing package
+import cv2 # OpenCV libaray for image processing 
+import imutils # Series of convenience functions to make OpenCV functions easier 
+import time # Time library 
+import math # Math library 
+import sys # System library 
+import os # Operating Systems Interface library 
+# =========== Standard Imports and Necessary Packages ===========
 
-
-#Adding file to Python PATH to callable elsewhere
-#sys.path.append(".")
-
-
-#class ObjectDetection():
-
-  #def __init__(self):
-    #pass:
-
-#def getObjectLocation():
-boundaries = [
-    ([17, 15, 100], [50, 56, 200]),
-    ([86, 31, 4], [220, 88, 50]),
-    ([25, 146, 190], [62, 174, 250]),
-    ([103, 86, 65], [145, 133, 128])
-]
+#Create double ended queue 
 pts = deque(maxlen = 32)
-cap = cv2.VideoCapture(1) #number is which camera on your system you want to use
-cap.set(3, 800)
+
+#Open the camera to capture a video stream 
+#If you are using a personal laptop, you may need to change the number from 1 to 0 or -1. 
+cap = cv2.VideoCapture(1) 
+
+#Define the height and width
+cap.set(3, 800) 
 cap.set(4, 600)
 
 #Allow the camera or video file to warm up
 time.sleep(2.0)
 
-#Math logic to find the height and length
+# =========== Math logic to find the height and length ===========
 depthOrAdjacent = 107
 
 verticalFieldOfView = 43.3
@@ -53,27 +58,31 @@ baseOrOppositeHFOV = math.tan(math.radians(horizontalFieldOfViewHalf)) * depthOr
 heightIn = baseOrOppositeVFOV * 2
 widthIn = baseOrOppositeHFOV * 2
 print(heightIn, widthIn) #Debug Statement
-count = 20
-zCounter = 0
-zAvg = 0
-total = 0
-os.system('cls||clear')   #to clear the termnial screen
 
+count = 20    # Counter for frames 
+zCounter = 0  # Counter for calcuating depth average 
+zAvg = 0      # Average depth over a the set of frames
+os.system('cls||clear')   #Clear the terminal screen 
+
+#Run the terminal 
 while True:
     print("=============================================")
     print("              Camera Terminal                ")
     print("=============================================\n")
-
+    
+    # Menu Options (Prompt User Input) 
     response = raw_input("0: Exit\n1: Capture single frame\n2: Continuous capture\n\n")
-    while((response != "0") and (response != "1") and (response != "2")):     # Input validation loop
+    # Input validation loop
+    while((response != "0") and (response != "1") and (response != "2")):     
             response = (raw_input("Please select a valid option.\n\n0: Exit\n1: Capture single frame\n2: Continuous capture\n\n"))
-
+    
+    # If response is 0 exit the program 
     if (response == "0"):
         exit(0)
-
+    
     if (response == "2"):
-        count = 200000000000
-
+        count = 20000000000
+    
     while True:
         #print(count)
         if(count < 0):
@@ -82,24 +91,16 @@ while True:
             count = 20
             zCounter = 0
             zAvg = 0
-            total = 0
           if(temp == "n"):
             print('\n')
             break
         while(count >= 0):
           count = count -1
-          #Ask the user to continue
-          #response = raw_input("To terminate put 'n': \n")
-          #if(response == "n"):
-            #cap.release()
-            #cv2.destroyAllWindows()
-            #break;
-          #count = count - 1
+          # Capture frame-by-frame
           _, img = cap.read()
-          #converting frame(img) from BGR (Blue-Green-Red) to HSV (hue-saturation-value)
-          image = img 
+          # Crop image to a desireable size (Captures table where object would be placed) 
           img = img[10:-10,10:-210]
-          #defining the range of color
+          # Defining the range of colors in HSV values for the filters 
           green_lower = np.array([25, 52, 72],np.uint8) 
           green_upper = np.array([102, 255, 255],np.uint8)
           red_lower = np.array([0, 100, 100], np.uint8)
@@ -109,40 +110,10 @@ while True:
           blue_lower = np.array([94, 80, 2], np.uint8)
           blue_upper = np.array([126, 255, 255], np.uint8)
           
-          #finding the range  color in the image
-          green = cv2.inRange(image, green_lower, green_upper)
-          red = cv2.inRange(image, red_lower, red_upper)
-          blue = cv2.inRange(image, blue_lower, blue_upper)
-          #res=cv2.bitwise_and(img, img, mask = yellow)
-          #track yellow and draw a rectangle. 
-          #(_,Green_contours,Green_hierarchy)=cv2.findContours(green,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-          #(_,Red_contours,Red_hierarchy)=cv2.findContours(red,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-          #(_,Blue_contours,Blue_hierarchy)=cv2.findContours(blue,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-          #if (_,Green_contours,Green_hierarchy) is not None: 
-            #f = open("color.txt", "w")
-            #f.write('%s' % "green")
-            #f.close()       
-            #print("Wrote green to file.")
-          #if (_,Red_contours,Red_hierarchy) is not None: 
-            #f = open("color.txt", "w")
-            #f.write('%s' % "red")
-            #f.close()    
-            #print("Wrote red to file")
-          #if (_,Blue_contours,Blue_hierarchy) is not None: 
-            #f = open("color.txt", "w")
-            #f.write('%s' % "blue")
-            #f.close()     
-            #print("Wrote blue to file.")      
-          #start code here
-          #Location of variable changed to fix results
-          #Croping image
-          #lefty, righty, leftx,rightx
-          #img = img[100:-20,10:-210]
           height, width, _ = img.shape
           inchesPerPixelH = heightIn / height #'''heightIn''' 
           inchesPerPixelW = widthIn / width #'''widthIn'''
-          #ends here
-
+          
           #DETECT BLUE
           hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
           mask_blue = cv2.inRange(hsv, blue_lower, blue_upper)
@@ -150,11 +121,11 @@ while True:
           gray = cv2.cvtColor(res_blue, cv2.COLOR_BGR2GRAY)
           # Blur using 3 * 3 kernel. 
           gray_blurred = cv2.blur(gray, (3, 3)) 
-        
           # Apply Hough transform on the blurred image. 
           detected_circles_blue = cv2.HoughCircles(gray_blurred,  
                             cv2.HOUGH_GRADIENT, 0.1, 20, param1 = 40, 
                         param2 = 23, minRadius = 0, maxRadius = 60) 
+          
         
           #DETECT GREEN
           mask_green = cv2.inRange(hsv, green_lower, green_upper)
@@ -166,6 +137,7 @@ while True:
           detected_circles_green = cv2.HoughCircles(gray_blurred,  
                             cv2.HOUGH_GRADIENT, 0.1, 20, param1 = 50, 
                         param2 = 25, minRadius = 6, maxRadius = 60) 
+          
           #DETECT RED
           mask_red = cv2.inRange(hsv, red_lower, red_upper)
           mask_red_T = cv2.inRange(hsv, red_lower_T, red_upper_T)
@@ -267,19 +239,17 @@ while True:
                         f = open("newZ.txt", "w")
                         f.write('%f' % zAvg)
                         f.close()
-                #My code ends here 
-          cv2.imshow("Detected Circle", img)
-          #cv2.imshow("blurred", gray_blurred)
-          #cv2.imshow("gray", gray)
-          cv2.imshow('Filter_blue', res_blue)
-          cv2.imshow('Filter_green', res_green)
-          cv2.imshow('Filter_red', res_red)
+                
+          cv2.imshow("Detected Circle", img)     # Open new window to show video stream with circle detection 
+          cv2.imshow('Filter_blue', res_blue)    # Open new window to show video stream that detects blue 
+          cv2.imshow('Filter_green', res_green)  # Open new window to show video stream that detects green 
+          cv2.imshow('Filter_red', res_red)      # Open new window to show video stream that detects red
           k = cv2.waitKey(30) & 0xff
           if k == 27:
 
             cap.release()
 
 
-         # close all windows
+         # Close all windows and exit the program 
             cv2.destroyAllWindows()
             exit(0)
