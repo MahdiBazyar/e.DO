0fa35df068c987e1b734e811583eb76eea4ce82d
Mahdi Bazyar <41386522+MahdiBazyar@users.noreply.github.com>
2020-03-24 23:11:10 -0400

    Update test.py
    Add description to the file

--- test.py
+++ test.py
@@ -1,5 +1,19 @@
-# Updates object position in simulation while training
-# belongs in direcrory /home/nvidia/jetson-reinforcement/build/aarch64
+# ================================== Test File! =============================================
+#  Developers: Wayne State University Senior Capstone Project Students
+#              Winter 2020: Hawraa Banoon, Nathaniel Smith, Kristina Stevoff, & Mahdi Bazyar
+#  Directory:  /home/nvidia/jetson-reinforcement/build/aarch64
+#  Interface:  Stores information about object's position for Gazebo simulation  
+#  Purpose:    This file updates the object's position in simulation model
+#              while we are training the virtual e.DO              
+#  Inputs:     3 Text Files
+#              newX.txt  (The distance of object from base of e.DO in cm) 
+#              xy.txt    (The angle that the object is in relation to e.DO in degree) 
+#              newZ.txt  (The depth of the object in cm)               
+#  Outputs:    Updated Version Of Previous 3 Text Files 
+#              newX.txt  (The distance of object from base of e.DO in cm) 
+#              xy.txt    (The angle that the object is in relation to e.DO in degree) 
+#              newZ.txt  (The depth of the object in cm) 
+# ================================== Test File! ==============================================
 
 import time
 import math
