ef8525c02e76726c497331fd748c37c3668ea6f9
nesmith4 <60016585+nesmith4@users.noreply.github.com>
2020-03-31 19:56:51 -0400

    Updated to use configuration file
    3-31-20
    7:56 PM

--- color_finalP1.py
+++ color_finalP1.py
@@ -5,7 +5,7 @@
 #  Directory:  /home/nvidia/jetson-reinforcement/build/aarch64
 #  Interface:  Runs the camera terminal 
 #  Purpose:    This file runs the camera and allows the user to choose capturing a
-#              single shot of 20 frames or run the camera continuously. The images 
+#              single shot of n frames or run the camera continuously. The images 
 #              will then be processed to output the color and location of the object.
 #  Inputs:     Camera connection
 #  Outputs:    4 Text Files
@@ -17,15 +17,16 @@
 
 
 # =========== Standard Imports and Necessary Packages ===========
-from collections import deque # import deque module provides you with a double ended queue
+from collections import deque # Import deque module provides you with a double ended queue
 from imutils.video import VideoStream # Capture a live video stream 
-import numpy as np # N-Demensional array-processing package
+import numpy as np # N-dimensional array-processing package
 import cv2 # OpenCV libaray for image processing 
 import imutils # Series of convenience functions to make OpenCV functions easier 
 import time # Time library 
 import math # Math library 
 import sys # System library 
 import os # Operating Systems Interface library 
+import cam_config as cfg # External configuration file
 # =========== Standard Imports and Necessary Packages ===========
 
 # Create double ended queue 
@@ -33,24 +34,24 @@ pts = deque(maxlen = 32)
 
 # Open the camera to capture a video stream 
 # If you are using a personal laptop, you may need to change the number from 1 to 0 or -1. 
-cap = cv2.VideoCapture(1) 
+cap = cv2.VideoCapture(cfg.capture_source) 
 
 # Define the height and width
-cap.set(3, 800) 
-cap.set(4, 600)
+cap.set(3, cfg.capture_width) 
+cap.set(4, cfg.capture_height)
 
 # Allow the camera or video file to warm up
-time.sleep(2.0)
+time.sleep(cfg.warmup_time)
 
 # =========== Math logic to find the height and length ===========
-depthOrAdjacent = 107
+depthOrAdjacent = cfg.surface_to_cam_cm
 
-verticalFieldOfView = 43.3
+verticalFieldOfView = cfg.cam_v_fov_cm
 verticalFieldOfViewHalf = verticalFieldOfView / 2
 baseOrOppositeVFOV = math.tan(math.radians(verticalFieldOfViewHalf)) * depthOrAdjacent
 
 
-horizontalFieldOfView = 70.42
+horizontalFieldOfView = cfg.cam_h_fov_cm
 horizontalFieldOfViewHalf = horizontalFieldOfView / 2
 baseOrOppositeHFOV = math.tan(math.radians(horizontalFieldOfViewHalf)) * depthOrAdjacent
 
@@ -59,10 +60,10 @@ heightIn = baseOrOppositeVFOV * 2
 widthIn = baseOrOppositeHFOV * 2
 print(heightIn, widthIn) # Debug Statement
 
-count = 20    # Counter for frames 
-zCounter = 0  # Counter for calcuating depth average 
-zAvg = 0      # Average depth over a the set of frames
-total = 0     # Total of the depth values of each frame
+count = cfg.number_of_frames    # Counter for frames 
+zCounter = 0                    # Counter for calcuating depth average 
+zAvg = 0                        # Average depth over a the set of frames
+total = 0                       # Total of the depth values of each frame
 
 continuousCamera = False
 
@@ -93,15 +94,15 @@ while True:
     
     while True:
         #print(count)
-        # If response is 1, capture 20 frames
+        # If response is 1, capture n frames
         if(count < 0 and response == "1"):
           temp = raw_input("Type (y) to get object location or (n) to exit: ")
           
           if(temp == "y"):
-            count = 20    # Counter for frames 
-            zCounter = 0  # Counter for calcuating depth average 
-            zAvg = 0      # Average depth over a the set of frames
-            total = 0     # Total of the depth values of each frame
+            count = cfg.number_of_frames    # Counter for frames 
+            zCounter = 0                    # Counter for calcuating depth average 
+            zAvg = 0                        # Average depth over a the set of frames
+            total = 0                       # Total of the depth values of each frame
             
           if(temp == "n"):
             print('\n')
@@ -113,17 +114,17 @@ while True:
           # Capture frame-by-frame
           _, img = cap.read()
           # Crop image to a desirable size (Captures table where object would be placed) 
-          img = img[10:-10,10:-210]
+          img = img[cfg.crop_row_start:cfg.crop_row_end,cfg.crop_col_start:cfg.crop_col_end]
           
           # Defining the range of colors in HSV values for the filters 
-          green_lower = np.array([25, 52, 72],np.uint8) 
-          green_upper = np.array([102, 255, 255],np.uint8)
-          red_lower = np.array([0, 100, 100], np.uint8)
-          red_upper = np.array([10, 255, 255], np.uint8)
-          red_lower_T = np.array([160, 100, 100], np.uint8)
-          red_upper_T = np.array([179, 255, 255], np.uint8)
-          blue_lower = np.array([94, 80, 2], np.uint8)
-          blue_upper = np.array([126, 255, 255], np.uint8)
+          green_lower = np.array([cfg.green_lower_hue, cfg.green_lower_saturation, cfg.green_lower_value],np.uint8) 
+          green_upper = np.array([cfg.green_upper_hue, cfg.green_upper_saturation, cfg.green_upper_value],np.uint8)
+          red_lower = np.array([cfg.red_lower_hue, cfg.red_lower_saturation, cfg.red_lower_value], np.uint8)
+          red_upper = np.array([cfg.red_upper_hue, cfg.red_upper_saturation, cfg.red_upper_value], np.uint8)
+          red_lower_T = np.array([cfg.red_lower_hue_2, cfg.red_lower_saturation_2, cfg.red_lower_value_2], np.uint8)
+          red_upper_T = np.array([cfg.red_upper_hue_2, cfg.red_upper_saturation_2, cfg.red_upper_value_2], np.uint8)
+          blue_lower = np.array([cfg.blue_lower_hue, cfg.blue_lower_saturation, cfg.blue_lower_value], np.uint8)
+          blue_upper = np.array([cfg.blue_upper_hue, cfg.blue_upper_saturation, cfg.blue_upper_value], np.uint8)
           
           height, width, _ = img.shape
           inchesPerPixelH = heightIn / height #'''heightIn''' 
@@ -134,36 +135,38 @@ while True:
           mask_blue = cv2.inRange(hsv, blue_lower, blue_upper)
           res_blue = cv2.bitwise_and(img, img, mask = mask_blue)
           gray = cv2.cvtColor(res_blue, cv2.COLOR_BGR2GRAY)
-          # Blur using 3 * 3 kernel. 
-          gray_blurred = cv2.blur(gray, (3, 3)) 
+          # Blur using n * n kernel. 
+          gray_blurred = cv2.blur(gray, (cfg.blur_kernel_size, cfg.blur_kernel_size)) 
           # Apply Hough transform on the blurred image. 
           detected_circles_blue = cv2.HoughCircles(gray_blurred,  
-                            cv2.HOUGH_GRADIENT, 0.1, 20, param1 = 40, 
-                        param2 = 23, minRadius = 0, maxRadius = 60) 
-          
-        
+                            cv2.HOUGH_GRADIENT, cfg.inverse_resolution_ratio, cfg.center_min_distance, 
+                            param1 = cfg.edge_upper_threshold, param2 = cfg.center_detect_threshold, 
+                            minRadius = cfg.min_radius, maxRadius = cfg.max_radius)
+                 
           # ================== DETECT GREEN CIRCLE =================
           mask_green = cv2.inRange(hsv, green_lower, green_upper)
           res_green = cv2.bitwise_and(img, img, mask = mask_green)
           gray = cv2.cvtColor(res_green, cv2.COLOR_BGR2GRAY)
-          # Blur using 3 * 3 kernel. 
-          gray_blurred = cv2.blur(gray, (3, 3)) 
+          # Blur using n * n kernel. 
+          gray_blurred = cv2.blur(gray, (cfg.blur_kernel_size, cfg.blur_kernel_size)) 
           # Apply Hough transform on the blurred image. 
           detected_circles_green = cv2.HoughCircles(gray_blurred,  
-                            cv2.HOUGH_GRADIENT, 0.1, 20, param1 = 40, 
-                        param2 = 23, minRadius = 6, maxRadius = 60) 
+                            cv2.HOUGH_GRADIENT, cfg.inverse_resolution_ratio, cfg.center_min_distance, 
+                            param1 = cfg.edge_upper_threshold, param2 = cfg.center_detect_threshold, 
+                            minRadius = cfg.min_radius, maxRadius = cfg.max_radius)
           
           # ================== DETECT RED CIRCLE =================
           mask_red = cv2.inRange(hsv, red_lower, red_upper)
           mask_red_T = cv2.inRange(hsv, red_lower_T, red_upper_T)
           res_red = cv2.bitwise_and(img, img, mask = mask_red | mask_red_T)
           gray = cv2.cvtColor(res_red, cv2.COLOR_BGR2GRAY)
-          # Blur using 3 * 3 kernel. 
-          gray_blurred = cv2.blur(gray, (3, 3)) 
+          # Blur using n * n kernel. 
+          gray_blurred = cv2.blur(gray, (cfg.blur_kernel_size, cfg.blur_kernel_size)) 
           # Apply Hough transform on the blurred image. 
           detected_circles_red = cv2.HoughCircles(gray_blurred,  
-                            cv2.HOUGH_GRADIENT, 0.1, 20, param1 = 40, 
-                        param2 = 23, minRadius = 6, maxRadius = 60) 
+                            cv2.HOUGH_GRADIENT, cfg.inverse_resolution_ratio, cfg.center_min_distance, 
+                            param1 = cfg.edge_upper_threshold, param2 = cfg.center_detect_threshold, 
+                            minRadius = cfg.min_radius, maxRadius = cfg.max_radius)
                        
           # WRITE COLOR DETECTED TO TEXT FILE
           if detected_circles_green is not None: 
@@ -183,7 +186,7 @@ while True:
             print("Wrote red to file.")      
             
         # If any color circles detected then must perform calculations for coordinates: 
-          if ((detected_circles_blue is not None)or(detected_circles_green is not None)or(detected_circles_red is not None)): 
+          if ((detected_circles_blue is not None) or (detected_circles_green is not None) or (detected_circles_red is not None)): 
               if detected_circles_blue is not None: 
                   detected_circles = np.uint16(np.around(detected_circles_blue))
               if detected_circles_green is not None:
@@ -196,31 +199,31 @@ while True:
                   a, b, r = pt[0], pt[1], pt[2] 
                   
                   # Draw a green circle around the circle detected 
-                  cv2.circle(img, (a, b), r, (0, 255, 0), 2) 
+                  cv2.circle(img, (a, b), r, (cfg.detected_circle_b, cfg.detected_circle_g, cfg.detected_circle_r), 2) 
 
                   # Draw a small red circle (of radius 1) to show the center
-                  cv2.circle(img, (a, b), 1, (0, 0, 255), 3)
+                  cv2.circle(img, (a, b), 1, (cfg.detected_center_b, cfg.detected_center_g, cfg.detected_center_r), 3)
                   pts.appendleft((a, b))
                   
-                  # loop over the set of tracked points
+                  # Loop over the set of tracked points
                   for i in np.arange(1, len(pts)):
-                      x = round(7.25 + (pts[0][0] * inchesPerPixelH), 2)
-                      y = round(((heightIn/2) - (pts[0][1] * inchesPerPixelH) + 4.5), 2)
+                      x = round(cfg.distance_buffer + (pts[0][0] * inchesPerPixelH), 2)
+                      y = round(((heightIn / 2) - (pts[0][1] * inchesPerPixelH) + cfg.angle_buffer), 2)
                       z = round((r * inchesPerPixelH) * 2, 2)
                       # BEGIN: MATH TO CALCULATE DEPTH OF OBJECT
                       # Source: https://www.pyimagesearch.com/2015/01/19/find-distance-camera-objectmarker-using-python-opencv/
                       if (r > 0):
-                        # 4 Cm by 4 Cm object 
+                        # n x n cm object 
                         # Depth Perception calculated by: D = (W x F) / P 
                         # D = Depth, F = Focal Length, P = pixels at certain length 
                         # In this case F = (P X D) / W , Where P = 16 pixels when on table, 
                         # W is 4 cm object, F = 107 cm from table to camera
-                        newZ = round((4 * 856) / (r * 2), 2) 
-                        newZ = 107 - newZ
+                        newZ = round((cfg.object_size_cm * 856) / (r * 2), 2) 
+                        newZ = cfg.surface_to_cam_cm - newZ
                       else:
-                        newZ = 6
+                        newZ = cfg.default_surface_depth
                         
-                      #Calculate the average depth for 20 frames captured 
+                      # Calculate the average depth for the frames captured 
                       zCounter = zCounter + 1
                       total = total + newZ
                       zAvg = total / zCounter
@@ -229,11 +232,11 @@ while True:
                       
                       # END: MATH TO CALCULATE DEPTH OF OBJECT 
                       
-                      #Convert from cartesian coordinates to 3D Polar Coordinates 
-                      newX = round(math.sqrt((x*x) + (y*y) +(zAvg*zAvg)), 2)
-                      xy = math.degrees(math.atan(y/x))
+                      # Convert from cartesian coordinates to 3D Polar Coordinates 
+                      newX = round(math.sqrt((x * x) + (y * y) +(zAvg * zAvg)), 2)
+                      xy = math.degrees(math.atan(y / x))
                       
-                      #PRIOR calculation from cartesian cordiantes to 2D Polar Coordinates 
+                      # PRIOR calculation from cartesian cordiantes to 2D Polar Coordinates 
                       #xy = xy -45.0
                       #xy = math.degrees(math.acos(((x*x) + (newX * newX) - (y*y)) / (2 * x * newX)))
                       
@@ -246,28 +249,28 @@ while True:
                         0.35, (0, 0, 255), 1)
                       
                       # Write values to text files to be read by ML model 
-                      if(x > 9):
+                      if(x > cfg.min_object_distance):
                         f = open("newX.txt", "w")
                         f.write('%f' % newX)
                         f.close()               
                         f = open("xy.txt", "w")
                         f.write('%f' % xy)
                         f.close()
-                      if(newZ > 5):
+                      if(newZ > cfg.default_surface_depth):
                         zAvg = zAvg + 2
                         f = open("newZ.txt", "w")
                         f.write('%f' % zAvg)
                         f.close()
                       else: 
-                        zAvg = 5.0
+                        zAvg = cfg.default_surface_depth
                         f = open("newZ.txt", "w")
                         f.write('%f' % zAvg)
                         f.close()
                 
           cv2.imshow("Detected Circle", img)     # Open new window to show video stream with circle detection 
-          cv2.imshow('Filter_blue', res_blue)    # Open new window to show video stream that detects blue 
-          cv2.imshow('Filter_green', res_green)  # Open new window to show video stream that detects green 
-          cv2.imshow('Filter_red', res_red)      # Open new window to show video stream that detects red
+          cv2.imshow('Blue Filter', res_blue)    # Open new window to show video stream that detects blue 
+          cv2.imshow('Green Filter', res_green)  # Open new window to show video stream that detects green 
+          cv2.imshow('Red Filter', res_red)      # Open new window to show video stream that detects red
           
           k = cv2.waitKey(30) & 0xff
           if k == 27:
