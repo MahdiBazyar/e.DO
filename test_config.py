f9fd0fe6b335f21b19f90d23bdcfb001b668e9bd
nesmith4 <60016585+nesmith4@users.noreply.github.com>
2020-04-02 14:54:01 -0400

    Configuration file for test.py
    4-2-20
    2:53 PM

---
+++ test_config.py
@@ -0,0 +1,22 @@
+''' Configuration file for test.py; keep in same directory '''
+
+# ===== Object bounaries =====
+x_min = 30
+x_max = 75
+
+y_min = -40
+y_max = 40
+
+z_min = 6
+z_max = 75
+# ============================
+
+
+# === Repositioning method ===
+mode = 2
+# ============================
+
+
+# ==== Repositioning time ====
+update_interval = 3600
+# ============================
